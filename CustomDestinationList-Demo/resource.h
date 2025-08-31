50046e3856b18f0f3fc56362e3e84911e943d16f
Stephen Foster <67705843+Stehfyn@users.noreply.github.com>
2025-08-31 14:15:48 -0700

    Update resource.h

--- CustomDestinationList-Demo/resource.h
+++ CustomDestinationList-Demo/resource.h
@@ -3,7 +3,20 @@
 // Used by CustomDestinationList-Demo.rc
 //
 #define IDI_ICON1                       101
-
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WIN32CUSTOMMENUBARAEROTHEME 107
+#define IDI_SMALL                       108
+#define IDC_WIN32CUSTOMMENUBARAEROTHEME 109
+#define IDR_MAINFRAME                   128
+#define ID_RIGHT_ONE                    32771
+#define ID_RIGHT_TWO                    32772
+#define ID_GRAYED                       32773
+#define ID_DISABLED                     32774
+#define ID_ANOTHER_ONE                  32775
+#define ID_ANOTHER_TWO                  32776
+#define IDC_STATIC                      -1
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
