f4d25046df252a3f07a03253af4fb6c556d1508c
Stephen Foster <67705843+Stehfyn@users.noreply.github.com>
2025-08-31 14:25:05 -0700

    Update demo.c

--- CustomDestinationList-Demo/demo.c
+++ CustomDestinationList-Demo/demo.c
@@ -1,14 +1,19 @@
 #include "CustomDestinationList.h"
+//#include "ImmersiveFlyouts.h"
 
 #include <dwmapi.h>
 #include <UxTheme.h>
+#include <vsstyle.h>
+
+#include "resource.h"
+
 #pragma comment (lib, "dwmapi")
 #pragma comment (lib, "UxTheme")
 
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 
 static LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
+static BOOL UAHWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam, LRESULT* lr);
 #if (defined NDEBUG)
 int
 _tWinMain(
@@ -192,6 +197,7 @@ void _tmain(void)
     wcx.hInstance     = (HINSTANCE)&__ImageBase;
     wcx.hbrBackground = CreateSolidBrush(RGB(43, 43, 43));
     wcx.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wcx.lpszMenuName  = MAKEINTRESOURCEW(IDC_WIN32CUSTOMMENUBARAEROTHEME);
     wcx.lpszClassName = _T("CustomDestinationList-DemoWndClass");
     wcx.hIcon         = LoadIcon((HINSTANCE)&__ImageBase, MAKEINTRESOURCE(101));
     szClassAtom       = (LPCTSTR)RegisterClassEx(&wcx);
@@ -298,8 +304,30 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 {
   static BOOL fMoving = FALSE;
   static BOOL fSizing = FALSE;
+  LRESULT lr = 0;
+
+  if (UAHWndProc(hwnd, uMsg, wParam, lParam, &lr))
+  {
+    return lr;
+  }
 
   switch (uMsg) {
+  case WM_COMMAND:
+  {
+    int wmId = LOWORD(wParam);
+    // Parse the menu selections:
+    switch (wmId)
+    {
+    case ID_ANOTHER_ONE:
+      break;
+    case IDM_EXIT:
+      DestroyWindow(hwnd);
+      break;
+    default:
+      return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+  }
+  break;
   case WM_NCCREATE:
   {
     HMODULE __uxtheme = GetModuleHandle(TEXT("UxTheme.dll"));
@@ -353,6 +381,12 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
     }
     break;
   }
+  case WM_NCACTIVATE:
+  {
+    if (wParam)
+      wParam = 0;
+    break;
+  }
   case WM_WINDOWPOSCHANGING:
   {
     ((LPWINDOWPOS)lParam)->flags |= SWP_DEFERERASE;
@@ -367,4 +401,241 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
   }
 
   return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+//static HBRUSH g_brBarBackground = CreateSolidBrush(RGB(0x80, 0x80, 0xFF));
+// window messages related to menu bar drawing
+#define WM_UAHDESTROYWINDOW    0x0090	// handled by DefWindowProc
+#define WM_UAHDRAWMENU         0x0091	// lParam is UAHMENU
+#define WM_UAHDRAWMENUITEM     0x0092	// lParam is UAHDRAWMENUITEM
+#define WM_UAHINITMENU         0x0093	// handled by DefWindowProc
+#define WM_UAHMEASUREMENUITEM  0x0094	// lParam is UAHMEASUREMENUITEM
+#define WM_UAHNCPAINTMENUPOPUP 0x0095	// handled by DefWindowProc
+
+// describes the sizes of the menu bar or menu item
+typedef union tagUAHMENUITEMMETRICS
+{
+  // cx appears to be 14 / 0xE less than rcItem's width!
+  // cy 0x14 seems stable, i wonder if it is 4 less than rcItem's height which is always 24 atm
+  struct {
+    DWORD cx;
+    DWORD cy;
+  } rgsizeBar[2];
+  struct {
+    DWORD cx;
+    DWORD cy;
+  } rgsizePopup[4];
+} UAHMENUITEMMETRICS;
+
+// not really used in our case but part of the other structures
+typedef struct tagUAHMENUPOPUPMETRICS
+{
+  DWORD rgcx[4];
+  DWORD fUpdateMaxWidths : 2; // from kernel symbols, padded to full dword
+} UAHMENUPOPUPMETRICS;
+
+// hmenu is the main window menu; hdc is the context to draw in
+typedef struct tagUAHMENU
+{
+  HMENU hmenu;
+  HDC hdc;
+  DWORD dwFlags; // no idea what these mean, in my testing it's either 0x00000a00 or sometimes 0x00000a10
+} UAHMENU;
+
+// menu items are always referred to by iPosition here
+typedef struct tagUAHMENUITEM
+{
+  int iPosition; // 0-based position of menu item in menubar
+  UAHMENUITEMMETRICS umim;
+  UAHMENUPOPUPMETRICS umpm;
+} UAHMENUITEM;
+
+// the DRAWITEMSTRUCT contains the states of the menu items, as well as
+// the position index of the item in the menu, which is duplicated in
+// the UAHMENUITEM's iPosition as well
+typedef struct UAHDRAWMENUITEM
+{
+  DRAWITEMSTRUCT dis; // itemID looks uninitialized
+  UAHMENU um;
+  UAHMENUITEM umi;
+} UAHDRAWMENUITEM;
+
+// the MEASUREITEMSTRUCT is intended to be filled with the size of the item
+// height appears to be ignored, but width can be modified
+typedef struct tagUAHMEASUREMENUITEM
+{
+  MEASUREITEMSTRUCT mis;
+  UAHMENU um;
+  UAHMENUITEM umi;
+} UAHMEASUREMENUITEM;
+void UAHDrawMenuNCBottomLine(HWND hWnd)
+{
+    MENUBARINFO mbi = { sizeof(mbi) };
+    if (!GetMenuBarInfo(hWnd, OBJID_MENU, 0, &mbi))
+    {
+        return;
+    }
+
+    RECT rcClient = { 0 };
+    GetClientRect(hWnd, &rcClient);
+    MapWindowPoints(hWnd, NULL, (POINT*)&rcClient, 2);
+
+    RECT rcWindow = { 0 };
+    GetWindowRect(hWnd, &rcWindow);
+
+    OffsetRect(&rcClient, -rcWindow.left, -rcWindow.top);
+
+    // the rcBar is offset by the window rect
+    RECT rcAnnoyingLine = rcClient;
+    rcAnnoyingLine.bottom = rcAnnoyingLine.top;
+    rcAnnoyingLine.top--;
+
+    
+    HDC hdc = GetWindowDC(hWnd);
+    HBRUSH hbr = CreateSolidBrush(RGB(43, 43, 43));
+    FillRect(hdc, &rcAnnoyingLine, hbr);
+    DeleteBrush(hbr);
+    ReleaseDC(hWnd, hdc);
+}
+
+static HTHEME g_menuTheme = NULL;
+static HBRUSH g_brItemBackground;
+static HBRUSH g_brItemBackgroundHot;
+static HBRUSH g_brItemBackgroundSelected;
+static HBRUSH g_brItemBorder;
+// processes messages related to UAH / custom menubar drawing.
+// return true if handled, false to continue with normal processing in your wndproc
+static BOOL UAHWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam, LRESULT* lr)
+{
+    switch (message)
+    {
+    case WM_UAHDRAWMENU:
+    {
+        UAHMENU* pUDM = (UAHMENU*)lParam;
+        RECT rc = { 0 };
+
+        // get the menubar rect
+        {
+            MENUBARINFO mbi = { sizeof(mbi) };
+            GetMenuBarInfo(hWnd, OBJID_MENU, 0, &mbi);
+
+            RECT rcWindow;
+            GetWindowRect(hWnd, &rcWindow);
+
+            // the rcBar is offset by the window rect
+            rc = mbi.rcBar;
+            OffsetRect(&rc, -rcWindow.left, -rcWindow.top);
+        }
+        
+        HBRUSH hbr = CreateSolidBrush(RGB(43, 43, 43));
+        FillRect(pUDM->hdc, &rc, hbr);
+        DeleteBrush(hbr);
+
+        return 1;
+    }
+    case WM_UAHDRAWMENUITEM:
+    {
+        UAHDRAWMENUITEM* pUDMI = (UAHDRAWMENUITEM*)lParam;
+
+        // ugly colors for illustration purposes
+
+
+        HBRUSH* pbrBackground = &g_brItemBackground;
+        HBRUSH* pbrBorder = &g_brItemBackground;
+
+        // get the menu item string
+        wchar_t menuString[256] = { 0 };
+        MENUITEMINFO mii = { sizeof(mii), MIIM_STRING };
+        {
+            mii.dwTypeData = menuString;
+            mii.cch = (sizeof(menuString) / 2) - 1;
+
+            GetMenuItemInfo(pUDMI->um.hmenu, pUDMI->umi.iPosition, TRUE, &mii);
+        }
+
+        // get the item state for drawing
+
+        DWORD dwFlags = DT_CENTER | DT_SINGLELINE | DT_VCENTER;
+
+        int iTextStateID = 0;
+        int iBackgroundStateID = 0;
+        {
+            if ((pUDMI->dis.itemState & ODS_INACTIVE) || (pUDMI->dis.itemState & ODS_DEFAULT)) {
+                // normal display
+                iTextStateID = MBI_NORMAL;
+                iBackgroundStateID = MBI_NORMAL;
+            }
+            if ((pUDMI->dis.itemState & ODS_HOTLIGHT) && !(pUDMI->dis.itemState & ODS_INACTIVE)) {
+                // hot tracking
+                iTextStateID = MBI_HOT;
+                iBackgroundStateID = MBI_HOT;
+
+                pbrBackground = &g_brItemBackgroundHot;
+                pbrBorder = &g_brItemBorder;
+            }
+            if (pUDMI->dis.itemState & ODS_SELECTED) {
+                // clicked
+                iTextStateID = MBI_PUSHED;
+                iBackgroundStateID = MBI_PUSHED;
+
+                pbrBackground = &g_brItemBackgroundSelected;
+                pbrBorder = &g_brItemBorder;
+            }
+            if ((pUDMI->dis.itemState & ODS_GRAYED) || (pUDMI->dis.itemState & ODS_DISABLED) || (pUDMI->dis.itemState & ODS_INACTIVE)) {
+                // disabled / grey text / inactive
+                iTextStateID = MBI_DISABLED;
+                iBackgroundStateID = MBI_DISABLED;
+            }
+            if (pUDMI->dis.itemState & ODS_NOACCEL) {
+                dwFlags |= DT_HIDEPREFIX;
+            }
+        }
+
+        if (!g_menuTheme) {
+            g_menuTheme = OpenThemeData(hWnd, L"Menu");
+            g_brItemBackground = CreateSolidBrush(RGB(43, 43, 43));
+            g_brItemBackgroundHot = CreateSolidBrush(RGB(43, 43, 43));
+            g_brItemBackgroundSelected = CreateSolidBrush(RGB(0xE0, 0xE0, 0xFF));
+            g_brItemBorder = CreateSolidBrush(RGB(0xB0, 0xB0, 0xFF));
+        }
+
+        DTTOPTS opts = { sizeof(opts), DTT_TEXTCOLOR, iTextStateID != MBI_DISABLED ? RGB(0xFF, 0xFF, 0xFF) : RGB(0x40, 0x40, 0x40) };
+
+        FillRect(pUDMI->um.hdc, &pUDMI->dis.rcItem, *pbrBackground);
+        FrameRect(pUDMI->um.hdc, &pUDMI->dis.rcItem, *pbrBorder);
+        DrawThemeTextEx(g_menuTheme, pUDMI->um.hdc, MENU_BARITEM, MBI_NORMAL, menuString, mii.cch, dwFlags, &pUDMI->dis.rcItem, &opts);
+
+        return 1;
+    }
+    case WM_UAHMEASUREMENUITEM:
+    {
+        UAHMEASUREMENUITEM* pMmi = (UAHMEASUREMENUITEM*)lParam;
+
+        // allow the default window procedure to handle the message
+        // since we don't really care about changing the width
+        *lr = DefWindowProc(hWnd, message, wParam, lParam);
+
+        // but we can modify it here to make it 1/3rd wider for example
+        pMmi->mis.itemWidth = (pMmi->mis.itemWidth * 4) / 3;
+
+        return 1;
+    }
+    case WM_THEMECHANGED:
+    {
+        if (g_menuTheme) {
+            CloseThemeData(g_menuTheme);
+            g_menuTheme = NULL;
+        }
+        // continue processing in main wndproc
+        return 0;
+    }
+    case WM_NCPAINT:
+    case WM_NCACTIVATE:
+        *lr = DefWindowProc(hWnd, message, wParam, lParam);
+        UAHDrawMenuNCBottomLine(hWnd);
+        return 1;
+        break;
+    default:
+        return 0;
+    }
 }
